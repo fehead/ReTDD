59fc2ceefad8169254683d76aa6554f81f217631
chaehoon <chaehoon@192.168.0.107>
2019-02-23 22:42:06 +0900

    로또 등수 등록.

--- src/main/java/kr/retdd/lotto/Lotto.java
+++ src/main/java/kr/retdd/lotto/Lotto.java
@@ -5,25 +5,55 @@ import java.util.List;
 import java.util.Random;
 
 public class Lotto {
-	static final int	MIN_LOTTO_NUMBER = 1;
-	static final int	MAX_LOTTO_NUMBER = 45;
+	public static final int	MAX_LOTTO_NUMBER = 45;
+	public	static final int	WIN_LOTTO_SIZE = 6;
 	
-	static final int	WIN_LOTTO_SIZE = 7;
+	private	static final int	MIN_LOTTO_NUMBER = 1;
 
 	private	Random generator = new Random();
 	private	List<Integer> candiNumbers;
+	private	List<Integer> lottoNumbers;
+	private	Integer			bonusNumber;
 
 	public Lotto() {
 		initCandidateNumbers();
 	}
 
-	public List<Integer> pickNumbers() {
-		List<Integer> ret = new ArrayList<Integer>();
+	public void pickNumbers() {
+		lottoNumbers = new ArrayList<Integer>();
 		for(int i = 0 ; i < WIN_LOTTO_SIZE ; ++i)
-			ret.add(pickNumber());
-		return ret;
+			addLottoNumber(pickNumber());
+		setBonusNumber(pickNumber());
 	}
 
+	public List<Integer> getLottoNumbers() {
+		return lottoNumbers;
+	}
+
+	public Integer getBonusNumber() {
+		return bonusNumber;
+	}
+	
+	public void printNumbers() {
+		System.out.print("LottoNumber: ");
+		for(Integer l : lottoNumbers)
+			System.out.print(l + " ");
+		
+		System.out.print("Bonus: " + this.bonusNumber);
+		System.out.println("");
+	}
+	
+	private void addLottoNumber(Integer newNumber) {
+		for(int i = 0 ; i < getLottoNumbers().size() ; ++i) {
+			Integer l = getLottoNumbers().get(i);
+			if(newNumber < l) {
+				getLottoNumbers().add(i, newNumber);
+				return;
+			}
+		}
+		getLottoNumbers().add(newNumber);
+	}
+	
 	private void initCandidateNumbers() {
 		candiNumbers = new ArrayList<>();
 		for(int i = MIN_LOTTO_NUMBER ; i <= MAX_LOTTO_NUMBER ; ++i)
@@ -36,6 +66,34 @@ public class Lotto {
 	
 	private int pickNumber() {
 		return candiNumbers.remove(getRandomIndex(candiNumbers.size()));
+	}
+
+	private void setBonusNumber(Integer bonusNumber) {
+		this.bonusNumber = bonusNumber;
+	}
+
+	public Integer lottery(List<Integer> numbers) {
+		int rightCnt = 0;
+		for(Integer n : numbers) {
+			if(getLottoNumbers().stream().anyMatch(l -> l == n))
+				rightCnt++;
+		}
+		
+		switch(rightCnt) {
+		case 3:
+			return 5;
+		case 4:
+			return 4;
+		case 5:
+			if(numbers.stream().anyMatch(l -> l == getBonusNumber()))
+				return 2;
+			return 3;
+		case 6:
+			return 1;
+		default:
+			return 0;
+		}
+
 	}
 
 }
--- src/test/java/kr/retdd/lotto/LottoTest.java
+++ src/test/java/kr/retdd/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package kr.retdd.lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
@@ -26,19 +27,121 @@ public class LottoTest {
 	@Test
 	public void 로또_6개나오게_하기() {
 		Lotto	lotto = new Lotto();
-		List<Integer>  lottoNumbers = lotto.pickNumbers();
+		lotto.pickNumbers();
+		List<Integer>  lottoNumbers = lotto.getLottoNumbers();
 		assertThat(lottoNumbers).isNotNull();
-		assertThat(lottoNumbers.size()).isEqualTo(7);
-		for(Integer l : lottoNumbers)
-			System.out.print(l + " ");
+		assertThat(lottoNumbers.size()).isEqualTo(6);
+		lotto.printNumbers();
 	}
 
 	@Test
 	public void test로또_중복_없는지_체크() {
 		Lotto	lotto = new Lotto();
-		List<Integer>  lottoNumbers = lotto.pickNumbers();
+		lotto.pickNumbers();
+		List<Integer>  lottoNumbers = lotto.getLottoNumbers();
 		Set<Integer>	numberSet = new HashSet<>(lottoNumbers);
+		numberSet.add(lotto.getBonusNumber());
 		assertThat(numberSet.size()).isEqualTo(7);
+		lotto.printNumbers();
 	}
+	
+	
+	@Test
+	public void 정렬_검사() {
+		Lotto	lotto = new Lotto();
+		lotto.pickNumbers();
+		List<Integer>  lottoNumbers = lotto.getLottoNumbers();
+		assertThat(lottoNumbers).isNotNull();
+		assertThat(lottoNumbers.size()).isEqualTo(6);
+		Integer	before = 0;
+				
+		for(Integer l : lottoNumbers) {
+			assertThat(l).isGreaterThan(before);
+			before = l;
+		}
+		
+		lotto.printNumbers();
+	}
+	
+	
+	@Test
+	public void 로또_등수_검사() {
+		Lotto	lotto = new Lotto();
+		lotto.pickNumbers();
+		List<Integer>  lottoNumbers = lotto.getLottoNumbers();
+		assertThat(lottoNumbers).isNotNull();
+		assertThat(lottoNumbers.size()).isEqualTo(6);
+		lotto.printNumbers();
+		
+		List<Integer> lottoR0 = 맞는것_가져오기(lottoNumbers, 0);
+		Integer r0 = lotto.lottery(lottoR0);
+		printNumbers("0개", lottoR0);
+		assertThat(r0).isEqualTo(0);
+
+		List<Integer> lottoR1 = 맞는것_가져오기(lottoNumbers, 1);
+		Integer r1 = lotto.lottery(lottoR1);
+		printNumbers("1개", lottoR1);
+		assertThat(r1).isEqualTo(0);
+
+		List<Integer> lottoR2 = 맞는것_가져오기(lottoNumbers, 2);
+		Integer r2 = lotto.lottery(lottoR2);
+		printNumbers("2개", lottoR2);
+		assertThat(r2).isEqualTo(0);
+
+		List<Integer> lottoR3 = 맞는것_가져오기(lottoNumbers, 3);
+		Integer r3 = lotto.lottery(lottoR3);
+		printNumbers("3개", lottoR3);
+		assertThat(r3).isEqualTo(5);
+
+		List<Integer> lottoR4 = 맞는것_가져오기(lottoNumbers, 4);
+		Integer r4 = lotto.lottery(lottoR4);
+		printNumbers("4개", lottoR4);
+		assertThat(r4).isEqualTo(4);
+
+		List<Integer> lottoR5 = 맞는것_가져오기(lottoNumbers, 5);
+		Integer r5 = lotto.lottery(lottoR5);
+		printNumbers("5개", lottoR5);
+		assertThat(r5).isEqualTo(3);
+
+		List<Integer> lottoR6 = 맞는것_가져오기(lottoNumbers, 6);
+		Integer r6 = lotto.lottery(lottoR6);
+		printNumbers("6개", lottoR6);
+		assertThat(r6).isEqualTo(1);
+
+		List<Integer> lottoRBouns = new ArrayList<>(lottoNumbers);
+		lottoRBouns.remove(0);
+		lottoRBouns.add(lotto.getBonusNumber());
+		Integer rBouns = lotto.lottery(lottoRBouns);
+		printNumbers("5개 + 보너스", lottoRBouns);
+		assertThat(rBouns).isEqualTo(2);
+		
+	}
+	
+	
+	private List<Integer> 맞는것_가져오기(List<Integer> lottoNumbers, int rightCnt) {
+		List<Integer> ret = new ArrayList<Integer>();
+		for(int i = 0 ; i < rightCnt ; ++i)
+			ret.add(lottoNumbers.get(i));
+		
+		for(int i = 1 ; i <= Lotto.MAX_LOTTO_NUMBER; ++i) {
+			if(ret.size() == Lotto.WIN_LOTTO_SIZE)
+				break;
+			if(!lottoIn(lottoNumbers, i))
+				ret.add(i);
+		}
+		
+		return ret;
+	}
+	
+	private boolean lottoIn(List<Integer> lottoNumbers, int num) {
+		return lottoNumbers.stream().anyMatch(l -> l == num);
+	}
+	
+	public void printNumbers(String msg, List<Integer> lottoNumbers) {
+		System.out.print(msg + " LottoNumber: ");
+		for(Integer l : lottoNumbers)
+			System.out.print(l + " ");
+		System.out.println("");
+	}	
 
 }
