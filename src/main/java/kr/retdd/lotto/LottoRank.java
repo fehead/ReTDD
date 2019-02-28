4dfb99ac21062ac34603500ebf6c35b88764b059
fehead <im.fehead@gmail.com>
2019-02-28 20:06:27 +0900

    LottoRank 구현중.

---
+++ src/main/java/kr/retdd/lotto/LottoRank.java
@@ -0,0 +1,18 @@
+package kr.retdd.lotto;
+
+public enum LottoRank {
+	RANK1(6, false),
+	RANK2(5, true),
+	RANK3(5, false),
+	RANK4(4, false),
+	RANK5(3, false);
+	
+	private	int		rightCnt;
+	private	boolean	eqBonusNum;
+	
+	LottoRank(int rightCnt, boolean eqBonusNum) {
+		this.rightCnt = rightCnt;
+		this.eqBonusNum = eqBonusNum;
+	}
+
+}
---
+++ src/test/java/kr/retdd/lotto/LottoRankTest.java
@@ -0,0 +1,13 @@
+package kr.retdd.lotto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LottoRankTest {
+
+	@Test
+	public void test() {
+	}
+
+}
