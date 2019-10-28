c6c75f6ef0713fe7e98e6d5b371a7505465f347b
chaehoon <chaehoon.leem@devmon.co.kr>
2019-10-28 12:31:36 +0900

    opt test...

--- src/main/java/kr/retdd/otp/OTPUtil.java
+++ src/main/java/kr/retdd/otp/OTPUtil.java
@@ -5,6 +5,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import javax.crypto.Mac;
@@ -12,43 +13,44 @@ import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base32;
 
+// 참고 : https://zero-gravity.tistory.com/221
 public class OTPUtil {
-	public static HashMap<String, String> generate(String name, String host) {
-        HashMap<String, String> map = new HashMap<String, String>();
-        byte[] buffer = new byte[5 + 5 * 5];        //버퍼에 할당
-        new Random().nextBytes(buffer);				//버퍼를 난수로 셋팅
+	public static String generateSecreKey() {
+        byte[] secretKey = new byte[10];        //버퍼에 할당
+        new Random().nextBytes(secretKey);		//버퍼를 난수로 셋팅
         Base32 codec = new Base32();
-        byte[] secretKey = Arrays.copyOf(buffer, 10);
         byte[] bEncodedKey = codec.encode(secretKey);
-
         String encodedKey = new String(bEncodedKey);        //생성된 Key (해당키로 OTP확인)
-
-        map.put("encodedKey", encodedKey);
-        map.put("url", host);
-
-        return map;
+        return encodedKey;
     }
 
-    //OTP코드 6자리 Check
-    public static boolean checkCode(String userCode, String otpkey) {
-        long otpnum = Long.parseLong(userCode); // OTP 앱에 표시되는 6자리 숫자
-        long wave = new Date().getTime() / 30000; // OTP의 주기(초)
-        boolean result = false;
-        try {
-             Base32 codec = new Base32();
-             byte[] decodedKey = codec.decode(otpkey);
-             int window = 3;
-             for (int i = -window; i <= window; ++i) {
-                 long hash = verify_code(decodedKey, wave + i);
-                 if (hash == otpnum) result = true;
-             }
-        } catch (InvalidKeyException | NoSuchAlgorithmException e) {
-             e.printStackTrace();
-        }
-     return result;
-    }
+	public static String getQRBarcodeURL(String user, String host, String secret) {
+		// QR코드 주소 생성
+		String fmt = "http://chart.apis.google.com/chart?cht=qr&chs=300x300"
+				 + "&chl=otpauth://totp/%s@%s%%3Fsecret%%3D%s&chld=H|0";
+		return String.format(fmt, user, host, secret);
+	}
+	
+	// OTP코드 6자리 Check
+	public static boolean checkCode(String userCode, String otpkey) {
+		int otpnum = Integer.parseInt(userCode); // OTP 앱에 표시되는 6자리 숫자
+		long wave = new Date().getTime() / 30000; // OTP의 주기(초)
+		try {
+			Base32 codec = new Base32();
+			byte[] decodedKey = codec.decode(otpkey);
+			int window = 3;
+			for (int i = -window; i <= window; ++i) {
+				long hash = verifyCode(decodedKey, wave + i);
+				if (hash == otpnum)
+					return true;
+			}
+		} catch (InvalidKeyException | NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 
-    private static int verify_code(byte[] key, long t) throws NoSuchAlgorithmException, InvalidKeyException {
+    private static int verifyCode(byte[] key, long t) throws NoSuchAlgorithmException, InvalidKeyException {
         byte[] data = new byte[8];
         long value = t;
         for (int i = 8; i-- > 0; value >>>= 8) {
@@ -75,6 +77,6 @@ public class OTPUtil {
         truncatedHash &= 0x7FFFFFFF;
         truncatedHash %= 1000000;
         
-        return (int) truncatedHash;
+        return (int)truncatedHash;
     }
 }
