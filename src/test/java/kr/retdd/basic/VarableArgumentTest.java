de349ccb8e07b159d8bd41c777983db1feb9f655
fehead <im.fehead@gmail.com>
2019-11-03 15:17:01 +0900

    Test VarableArgument.

--- src/test/java/kr/retdd/basic/VarableArgumentTest.java
+++ src/test/java/kr/retdd/basic/VarableArgumentTest.java
@@ -1,5 +1,7 @@
 package kr.retdd.basic;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class VarableArgumentTest {
@@ -12,9 +14,21 @@ public class VarableArgumentTest {
 			System.out.println(">> " + s);
 	}
 
+	public void var3(int a, String ...strings) {
+		String [] addedArr = Arrays.copyOf(strings, strings.length + 1);
+		addedArr[addedArr.length-1] = String.valueOf(a);
+		var2(addedArr);
+	}
+	
 	@Test
 	public void vArgu1() {
 		var1(0, "1", "2", "3", "4");
+	}
+
+	
+	@Test
+	public void vArgu2() {
+		var3(0, "1", "2", "3", "4");
 	}
 
 }
